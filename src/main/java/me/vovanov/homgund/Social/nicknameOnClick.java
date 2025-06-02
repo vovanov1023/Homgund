5f96a155a498e4b30ed51c5a66f94bbd64fe536a
vovanov1023 <vovanov10230@gmail.com>
2025-06-02 17:56:48 +0300

    return the old implementation just in case

--- src/main/java/me/vovanov/homgund/Social/nicknameOnClick.java
+++ src/main/java/me/vovanov/homgund/Social/nicknameOnClick.java
@@ -4,7 +4,8 @@ import net.kyori.adventure.text.TextComponent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.RayTraceResult;
 
 import static me.vovanov.homgund.Homgund.PLUGIN;
@@ -25,9 +26,20 @@ public class nicknameOnClick implements Listener {
                 maxDistance,
                 entity -> entity instanceof Player && !entity.equals(player)
         );
-        if (result != null && result.getHitEntity() instanceof Player targetPlayer) {
-            TextComponent name = getFormattedName(targetPlayer, true, false);
-            player.sendActionBar(name);
-        }
+        if (result != null && result.getHitEntity() instanceof Player targetPlayer) sendActionBar(player, targetPlayer);
+
+    }
+
+    @EventHandler
+    public void onPlayerInteractPlayer(PlayerInteractEntityEvent event) {
+        if (!(event.getRightClicked() instanceof Player targetPlayer)) return;
+        if (!event.getPlayer().getInventory().getItemInMainHand().isEmpty()) return;
+        Player player = event.getPlayer();
+        sendActionBar(player, targetPlayer);
+    }
+
+    private void sendActionBar(Player player, Player targetPlayer) {
+        TextComponent name = getFormattedName(targetPlayer, true, false);
+        player.sendActionBar(name);
     }
 }
